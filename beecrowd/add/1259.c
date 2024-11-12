f387c47c80eb807c7259b35db1ebc99e598c9884
Enrico Bertoni <em.bertoni@unifesp.br>
2024-11-12 11:10:28 -0300

    Create 1259.c

---
+++ beecrowd/add/1259.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp_par(const void *a, const void *b){
+    return (*(int*)a - *(int*)b);
+}
+
+int cmp_impar(const void *a, const void *b){
+    return (*(int*)b - *(int*)a);
+}
+
+
+int main(){
+    int n;
+    scanf("%i", &n);
+    
+    int num[n];
+    int par[n], impar[n];
+    int countp = 0, counti = 0;
+
+    for (int i = 0; i < n; i++){
+        scanf("%i", &num[i]);
+        if (num[i]%2 == 0){
+            par[countp++] = num[i];
+        }
+        else {
+            impar[counti++] = num[i];
+        }
+    }
+    qsort(par, countp, sizeof(int), cmp_par);
+    qsort(impar, counti, sizeof(int), cmp_impar);
+    for (int i = 0; i < countp; i++){
+        printf("%i\n", par[i]);
+    }
+    for (int i = 0; i < counti; i++){
+        printf("%i\n", impar[i]);
+    }
+    return 0;
+}
