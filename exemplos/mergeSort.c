05a1e391207f83d1781df1072a692d1cab408982
Enrico Bertoni <em.bertoni@unifesp.br>
2024-12-03 09:40:03 -0300

    Update mergeSort.c

--- exemplos/mergeSort.c
+++ exemplos/mergeSort.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+
+void vetRand(int v[], int n){
+    for (int i = 0; i < n; i++){
+        v[i] = rand() % 1000000;
+    }
+}
 
 void merge(int v[], int l, int m, int r){
     int n1 = m - l + 1;
@@ -45,4 +52,36 @@ void mergeSort(int v[], int l, int r){
         mergeSort(v, m + 1, r);
         merge(v, l , m, r);
     }
+}
+
+double mTempoMerge(void (*sortFunc)(int[], int, int), int v[], int n){
+    clock_t start = clock();
+    sortFunc(v, 0, n-1);
+    clock_t end = clock();
+    return (double)(end-start) / CLOCKS_PER_SEC;
+}
+
+int main(){
+    int tam[] = {50000, 100000, 150000, 200000, 250000, 300000, 350000, 400000};
+    int num = sizeof(tam) / sizeof(tam[0]);
+    int *vMerge;
+    printf("Tamanho\tMergeSort\n");
+    for (int i = 0; i < num; i++){
+        int n = tam[i];
+        vMerge = (int*)malloc(n * sizeof(int));
+        vetRand(vMerge, n);
+        
+        double timeMerge = 0;
+        for (int j = 0; j < 3; j++){
+            int *temp = (int*)malloc(n * sizeof(int));
+            memcpy(temp, vMerge, n * sizeof(int));
+            timeMerge += mTempoMerge(mergeSort, temp, n);
+            free(temp);
+        }
+        timeMerge /= 3;
+
+        printf("%i\t%lf\n", n, timeMerge);
+        free(vMerge);
+    }
+    return 0;
 }
